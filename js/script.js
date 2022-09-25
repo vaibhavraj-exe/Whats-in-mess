ec49e5b81d5fe94b0504bea3947c2073092c36e0
Vaibhav Raj <vraj.exe@gmail.com>
2022-09-25 18:59:34 +0530

    Bug fixes and performance improvements

--- js/script.js
+++ js/script.js
@@ -1,37 +1,38 @@
 
 let messDict =[
+    // Sunday
     [["Bread", "Butter", "Jam", "Chole Bhature", "Veg Upma", "Chutney", "Tea/Coffee/Milk"],
     ["Chapathi", "Chicken (Pepper / Kadai)", "Paneer Butter Masala", "Dal Dhadka", "Mint Pulao/Steamed Rice", "Rasam", "Poriyal", "Butter Milk", "Fryums", "Pickle"],
     ["Kara Sev / Pakoda", "Tea/Coffee"],
-    ["Chapathi", "Aloo gobi curry", "Steamed Rice", "Hara Moong Dal Tadka", "Karakozhambu", "Kootu", "Rasam", "Fryums", "Veg salad", "Milk", "Seasonal Fruit", "Ice cream / Cone Ice cream", "Non-Veg Gravy"]],
-
+    ["Aloo Paratha", "Aloo gobi curry", "Steamed Rice", "Moong Dal Tadka", "Karakozhambu", "Kootu", "Rasam", "Fryums", "Veg salad", "Milk", "Seasonal Fruit", "Ice cream / Cone Ice cream", "Non-Veg Gravy"]],
+    // Monday
     [["Butter", "Bread", "Jam", "Pongal", "Vada", "Chutney", "Sambhar", "Chapati", "White Peas Masala", "Tea/Coffee/Milk"], 
-    ["Sweet", "Methi Chapati", "Veg Topiyaza Curry", "Tomato Pulao/Steamed Rice", "Radish Sambar", "Panchratna Dal", "Aviyal", "Cabbage Baigan Aloo Sabji", "Rasam", "Buttermilk", "Pickle", "Fryums"],
+    ["Methi Chapati", "Veg Topiyaza Curry", "Tomato Pulao/Steamed Rice", "Sambar", "Panchratna Dal", "Aviyal", "Cabbage Baigan Aloo Sabji", "Rasam", "Buttermilk", "Pickle", "Fryums"],
     ["Pav Bhaji/Sweet Bun", "Tea/Coffee"],
-    ["Atta Paratha", "Channa Masala", "Vegetable Idly", "Special Chutney", "Steamed Rice", "Moong Dal Fry", "Aloo Jeera Dry", "Sambar", "Rasam", "Poriyal", "Fryums", "Veg Salad", "Milk", "Banana"]],
-
-    [["Bread", "Butter", "Jam", "Chapati", "Dal Aloo Masala", "Podi", "Dhosai", "Sambar", "Chutney", "Tea/Coffee/Milk"],
-    ["Sweet", "Poori", "Aloo Curry", "Bahara Pulao", "Idiyappam(Tomato/ Lemon)", "Curd Rice", "Pickle", "Fryums"], 
+    ["Atta Paratha", "Channa Masala", "Vegetable Idly", "Special Chutney", "Steamed Rice", "Moong Dal Fry", "Aloo Jeera Dry", "Sambar", "Rasam", "Fryums", "Veg Salad", "Milk", "Banana", "Fish Curry"]],
+    // Tuesday
+    [["Bread", "Butter", "Jam", "Chapati", "Rajma Masala", "Podi Dhosai", "Sambar", "Chutney", "Tea/Coffee/Milk"],
+    ["Sweet", "Poori", "Aloo Curry", "Bahara Pulao", "Idiyappam(Tomato/ Lemon)", "Steamed Rice", "Dal Fry", "Rasam", "Butter Milk", "Curd Rice", "Pickle", "Fryums"], 
     ["Bonda / Keerai Vada", "Tea / Coffee"],
-    ["Chilli Paratha", "Steamed Rice", "Dal Makani", "Sambar", "Rasam", "Poriyal", "Pickle", "Fryums", "Veg salad", "Milk", "Special Fruits", "Mutton Gravy"]],
-
-    [["Bread", "Butter", "Jam", "Idly", "Sambar", "Kara Chutney", "Poha", "Mint Chutney", "Tea/Coffee Milk", "Masala Omelete"],
-    ["Ceylon Paratha", "Meal Maker Curry", "Subji", "Veg Pulao/Steamed Rice", "Kadhamba Sambar", "Tomato Dal", "Garlic Rasam", "Poriyal", "Butter milk", "Pickle", "Fryums"],
+    ["Ceylon Paratha", "Green Peas Masala", "Steamed Rice", "Dal Makani", "Sambar", "Rasam", "Poriyal", "Pickle", "Fryums", "Veg salad", "Milk", "Special Fruits", "Mutton Gravy"]],
+    // Wednesday
+    [["Bread", "Butter", "Jam", "Idly", "Sambar", "Chutney", "Poha", "Mint Chutney", "Tea/Coffee Milk", "Masala Omelete"],
+    ["Chappathi", "Meal Maker Curry", "Subji", "Veg Pulao/Steamed Rice", "Kadhamba Sambar", "Tomato Dal", "Garlic Rasam", "Poriyal", "Butter milk", "Pickle", "Fryums"],
     ["Veg Puff / Sweet Puff", "Tea / Coffee"],
     ["Chapathi", "Steamed Rice", "Dal Tadka", "Butter Chicken Masala", "Paneer Mutter Masala / Malai Paneer", "Rasam", "Pickle", "Fryums", "Veg Salad", "Milk", "Banana", "Non Veg Gravy"]],
-
-    [["Bread", "Butter", "Jam", "Veg Semiya", "Kitchadi", "Chappathi", "Aloo Rajma", "Sambar", "Chutney", "Tea/Coffee/ Milk"],
+    // Thursday
+    [["Bread", "Butter", "Jam", "Veg Semiya Kitchadi", "Chappathi", "Aloo Rajma", "Chutney", "Tea/Coffee/ Milk"],
     ["Mint Chapathi", "Soya beans curry", "Onion Pulao / Steamed Rice", "Dal Makhani", "Kadi pakoda", "Rasam", "Aloo gobi masala", "Yam Varuval", "Buttermilk", "Pickle", "Fryums"],
     ["Bajji", "Tea / Coffee"],
     ["Madras Paratha", "Mattar Paneer Masala", "Steamed Rice", "Dal tadka", "Sambar", "Dosa Chutney", "Rasam", "Pickle", "Fryums", "Veg salad", "Milk" , "Banana", "Non-Veg Gravy"]],
-
+    // Friday
     [["Bread", "Butter",  "Jam", "Poori", "Potato Masala", "Wheat Upma", "Chutney", "Tea/Coffee/Milk"],
-    ["Sweet", "Chapathi", "Aloo Mattar Masala", "Maisoore Dal", "Veg Biyani", "Mix Raihta", "Variety Rice", "Thovaiyal", "Curd Rice", "Pickle",  "Fryums"],
+    ["Sweet", "Chapathi", "Aloo Mattar Masala", "Maisoore Dal", "Veg Biyani", "Mix Raihta", "Variety Rice", "Rasam", "Curd Rice", "Butter Milk", "Pickle",  "Fryums"],
     ["Cake / Fruit Cake", "Tea/Coffee"],
     ["Chapati", "Veg Manchurian", "Veg fried Rice (or) Veg Noodles", "Steamed Rice", "Dal Maharani", "Sambar", "Rasam", "Fryums", "Veg salad", "Milk", "Special Fruit", "Non-Veg Gravy"]],
-
+    // Saturday
     [["Bread", "Butter", "Jam", "Chappathi", "Veg Khorma", "Idiyappam (Tomato & Lemon)", "Chutney", "Tea/Coffee/Milk", "Boiled Egg"],
-    ["Poori", "White peas Curry", "Aloo Bindli sabji", "Bahara Pulao / Steamed Rice", "Dal Lasooni", "Paruppu urundai kozhambu", "Kootu", "Rasam", "Buttermilk", "Pickle", "Fryums"],
+    ["Poori", "White peas Curry", "Aloo Bindli sabji", "Bahara Pulao / Steamed Rice", "Tomato Dal", "Paruppu Urundai Kozhambu", "Kootu", "Rasam", "Buttermilk", "Pickle", "Fryums"],
     ["Samosa / Aloo Bonda", "Tea/Coffee"],
     ["Panjabi Paratha", "Aloo Capsicum Sabji", "Steamed Rice", "Rajma curry", "Masala Dal", "Idly", "Chutney", "Sambar", "Poriyal", "Rasam", "Pickle", "Fryums", "Veg salad", "Milk", "Seasonal Fruits", "Fish Gravy"]],
 
